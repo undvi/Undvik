58be5392468fa20c713cb6b26feabb7f59befe5c
undvi <muiton1@outlook.de>
2025-02-18 14:12:54 +0100

    schmieden

--- PersistentEmpiresLib/PersistentEmpiresLib/NetworkMessages/Client/RequestExecuteCraft.cs
+++ PersistentEmpiresLib/PersistentEmpiresLib/NetworkMessages/Client/RequestExecuteCraft.cs
@@ -49,9 +49,51 @@ namespace PersistentEmpiresLib.NetworkMessages.Server
             if (!playerBlueprints.Contains(ItemID))
             {
                 InformationManager.DisplayMessage(new InformationMessage($"❌ Fehler: Spieler {PlayerID} hat {ItemID} nicht erforscht!"));
+                GameNetwork.BeginModuleEventAsServer(PlayerID);
+                GameNetwork.WriteMessage(new CraftingRejected(PlayerID, ItemID));
+                GameNetwork.EndModuleEventAsServer();
                 return false;
             }
             return true;
+        }
+    }
+
+    [DefineGameNetworkMessageTypeForMod(GameNetworkMessageSendType.FromServer)]
+    public sealed class CraftingRejected : GameNetworkMessage
+    {
+        public int PlayerID { get; private set; }
+        public string ItemID { get; private set; }
+
+        public CraftingRejected() { }
+
+        public CraftingRejected(int playerId, string itemId)
+        {
+            this.PlayerID = playerId;
+            this.ItemID = itemId;
+        }
+
+        protected override MultiplayerMessageFilter OnGetLogFilter()
+        {
+            return MultiplayerMessageFilter.MissionObjects;
+        }
+
+        protected override string OnGetLogFormat()
+        {
+            return $"❌ Crafting-Anfrage abgelehnt: Spieler {PlayerID} kann {ItemID} nicht craften!";
+        }
+
+        protected override bool OnRead()
+        {
+            bool result = true;
+            this.PlayerID = GameNetworkMessage.ReadIntFromPacket(new CompressionInfo.Integer(0, 10000, true), ref result);
+            this.ItemID = GameNetworkMessage.ReadStringFromPacket(ref result);
+            return result;
+        }
+
+        protected override void OnWrite()
+        {
+            GameNetworkMessage.WriteIntToPacket(this.PlayerID, new CompressionInfo.Integer(0, 10000, true));
+            GameNetworkMessage.WriteStringToPacket(this.ItemID);
         }
     }
 }
